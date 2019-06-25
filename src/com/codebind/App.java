c47c519017929cc3da81e050fff807c307b6c129
Vinicius de Sousa Vilela <vsv@ug2c34.windows.cin.ufpe.br>
2019-06-25 10:01:48 -0300

    Criando a lista

---
+++ src/com/codebind/App.java
@@ -0,0 +1,44 @@
+package com.codebind;
+
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+import org.w3c.dom.*;
+
+import javax.swing.*;
+import javax.xml.soap.SOAPElement;
+import javax.xml.soap.SOAPException;
+import javax.xml.soap.Text;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class App {
+    private JButton button1;
+    private JPanel panel1;
+    private JTextField textField1;
+    private JList<String> list1;
+
+    public App() {
+
+        DefaultListModel model = new DefaultListModel();
+        list1 = new JList<> (model);
+
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+                model.addElement(textField1.getText());
+
+                textField1.setText("");
+            }
+        });
+
+    }
+
+    public static void main(String[] args){
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new App().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
