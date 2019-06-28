7a3bf0ae3ce32fa773bff33dd7bff0a0895119ea
viniciusssousav <vsv@cin.ufpe.br>
2019-06-27 22:10:19 -0300

    Scrollbar funcionando perfeitamente

--- src/com/codebind/App.java
+++ src/com/codebind/App.java
@@ -1,16 +1,7 @@
 package com.codebind;
 
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
-import javafx.scene.input.KeyCode;
-import org.w3c.dom.*;
-
 import javax.swing.*;
-import javax.xml.soap.SOAPElement;
-import javax.xml.soap.SOAPException;
-import javax.xml.soap.Text;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
@@ -18,40 +9,64 @@ public class App {
     private JButton button1;
     private JPanel panel1;
     private JTextField textField1;
-    private JList list1;
+    private JList list;
+    private JScrollPane scrollPane;
     private DefaultListModel listModel;
+
+
 
     private App() {
 
         listModel = new DefaultListModel();
 
         button1.addActionListener(actionEvent -> {
-
             if(!textField1.getText().isEmpty()){
                 listModel.addElement(textField1.getText());
-                list1.setModel(listModel);
+                list.setModel(listModel);
                 textField1.setText("");
+                //scrollPane.getVerticalScrollBar().setValue( scrollPane.getVerticalScrollBar().getMaximum() +1);
+                SwingUtilities.invokeLater(() -> {
+                    Dimension vpSize = scrollPane.getViewport().getExtentSize();
+                    Dimension logSize = list.getSize();
+
+                    int height = logSize.height - vpSize.height;
+
+                    scrollPane.getViewport().setViewPosition(new Point(0, height));
+
+                });
+
             }
         });
 
         textField1.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
-                if(e.getKeyCode() == KeyEvent.VK_ENTER && !textField1.getText().isEmpty()){
+                if(e.getKeyCode() == KeyEvent.VK_ENTER && !textField1.getText().isEmpty()) {
                     listModel.addElement(textField1.getText());
-                    list1.setModel(listModel);
+                    list.setModel(listModel);
                     textField1.setText("");
+                    //scrollPane.getVerticalScrollBar().setValue( scrollPane.getVerticalScrollBar().getMaximum() +1);
+                    SwingUtilities.invokeLater(() -> {
+                        Dimension vpSize = scrollPane.getViewport().getExtentSize();
+                        Dimension logSize = list.getSize();
+
+                        int height = logSize.height - vpSize.height;
+
+                        scrollPane.getViewport().setViewPosition(new Point(0, height));
+
+                    });
                 }
             }
         });
-
-
     }
 
     public static void main(String[] args){
         JFrame frame = new JFrame("App");
-        //frame.setSize(1000, 1000);
         frame.setContentPane(new App().panel1);
+        frame.setSize(1000, 1000);
+        frame.setMinimumSize(new Dimension(500,500));
+        frame.setMaximumSize(new Dimension(500,500));
+        frame.setPreferredSize(new Dimension(500,500));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
